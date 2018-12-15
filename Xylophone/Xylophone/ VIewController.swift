1116f4cffec4ddac6adbb4d90f9cb99945fc5e86
Tanmay2298 <Tanmaygulati@gmail.com>
2018-12-16 00:13:51 +0530

    Xylophone App using AVFoundation

---
+++ Xylophone/Xylophone/ VIewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    var audioPlayer : AVAudioPlayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        playSound(soundNumber : sender.tag)
+    }
+    func playSound(soundNumber : Int)
+    {
+        let soundUrl = Bundle.main.url(forResource : "note\(soundNumber)", withExtension : "wav")
+        do
+        {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundUrl!)
+        }
+        catch
+        {
+            print(error)
+        }
+        audioPlayer.play()
+    }
+}
+
