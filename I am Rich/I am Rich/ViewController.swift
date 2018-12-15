ee9b22ceada27147ef927cbf933bc59754adffcf
Tanmay2298 <Tanmaygulati@gmail.com>
2018-12-15 14:26:08 +0530

    I am Rich App Completed

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Tanmay Gulati on 15/12/18.
+//  Copyright © 2018 Tanmay Gulati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
